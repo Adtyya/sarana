5a86864b17e046f0f947875f2d4ec091a539bf52
aditya <aditdualapan@gmail.com>
2025-09-18 17:15:59 +0700

    wip

---
+++ types/tracking/tracking.ts
@@ -0,0 +1,34 @@
+// types/tracking.ts
+export interface DHLTimestamp {
+  TimestampCode: string;
+  TimestampDescription: string;
+  TimestampDateTime: string;
+  LocationCodeUN: string;
+  LocationName: string;
+}
+
+export interface DHLTrackingData {
+  ShipmentTracking: {
+    Shipment: {
+      HousebillNumber: string;
+      Origin: { LocationName: string; CountryCode: string };
+      Destination: { LocationName: string; CountryCode: string };
+      ProductType: string;
+      TotalPackages: string;
+      TotalWeight: { "*body": string; "@uom": string };
+      LastEvent: {
+        TimestampDateTime: string;
+        Location: string;
+        Description: string;
+      };
+      Timestamps: { Timestamp: DHLTimestamp[] };
+    };
+  };
+}
+
+export interface TrackingResponse {
+  shipment_id: string;
+  tracking_number: string;
+  courier: "DHL" | string;
+  tracking_data: DHLTrackingData;
+}
